df12e62e55ba107b573bcad11f0b9e789aa96c42
anujercyselin <43059403+anujercyselin@users.noreply.github.com>
2018-09-07 12:21:58 +0530

    Add files via upload

---
+++ float.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args){
+      int a;
+      double b,c,d,e,f;
+      a=2;
+      b=4;
+      c=a+b;
+      d=a-b;
+      e=a*b;
+      f=a/b;
+      System.out.println("add value:"+c);
+      System.out.println("sub value:"+d);
+      System.out.println("mul value:"+e);
+      System.out.println("div value:"+f);
+  }
+}
